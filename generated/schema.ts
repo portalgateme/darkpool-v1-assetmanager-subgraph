6bd3cb12a963d4194059dade49d2b5c4ec373f00
Peter0x2 <peter0x2@outlook.com>
2025-02-13 21:14:08 +0800

    add batchjoinsplit

--- generated/schema.ts
+++ generated/schema.ts
@@ -7061,3 +7061,112 @@ export class TorosWithdrawal extends Entity {
     this.set("transactionHash", Value.fromBytes(value));
   }
 }
+
+export class BatchJoinSplit extends Entity {
+  constructor(id: Bytes) {
+    super();
+    this.set("id", Value.fromBytes(id));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save BatchJoinSplit entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.BYTES,
+        `Entities of type BatchJoinSplit must have an ID of type Bytes but the id '${id.displayData()}' is of type ${id.displayKind()}`,
+      );
+      store.set("BatchJoinSplit", id.toBytes().toHexString(), this);
+    }
+  }
+
+  static loadInBlock(id: Bytes): BatchJoinSplit | null {
+    return changetype<BatchJoinSplit | null>(
+      store.get_in_block("BatchJoinSplit", id.toHexString()),
+    );
+  }
+
+  static load(id: Bytes): BatchJoinSplit | null {
+    return changetype<BatchJoinSplit | null>(
+      store.get("BatchJoinSplit", id.toHexString()),
+    );
+  }
+
+  get id(): Bytes {
+    let value = this.get("id");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set id(value: Bytes) {
+    this.set("id", Value.fromBytes(value));
+  }
+
+  get nullifiersIn(): Array<Bytes> {
+    let value = this.get("nullifiersIn");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytesArray();
+    }
+  }
+
+  set nullifiersIn(value: Array<Bytes>) {
+    this.set("nullifiersIn", Value.fromBytesArray(value));
+  }
+
+  get notesOut(): Array<Bytes> {
+    let value = this.get("notesOut");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytesArray();
+    }
+  }
+
+  set notesOut(value: Array<Bytes>) {
+    this.set("notesOut", Value.fromBytesArray(value));
+  }
+
+  get blockNumber(): BigInt {
+    let value = this.get("blockNumber");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set blockNumber(value: BigInt) {
+    this.set("blockNumber", Value.fromBigInt(value));
+  }
+
+  get blockTimestamp(): BigInt {
+    let value = this.get("blockTimestamp");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBigInt();
+    }
+  }
+
+  set blockTimestamp(value: BigInt) {
+    this.set("blockTimestamp", Value.fromBigInt(value));
+  }
+
+  get transactionHash(): Bytes {
+    let value = this.get("transactionHash");
+    if (!value || value.kind == ValueKind.NULL) {
+      throw new Error("Cannot return null for a required field.");
+    } else {
+      return value.toBytes();
+    }
+  }
+
+  set transactionHash(value: Bytes) {
+    this.set("transactionHash", Value.fromBytes(value));
+  }
+}
